ff91f26fe82b486f1c5b9b3cb45357e6dc6a30b0
Boubaker Khanfir <bkhanfir@exoplatform.com>
2019-10-14 11:49:29 +0100

    Rename package to remove addon from name (#4)

--- src/main/resources/truffle-config.js
+++ src/main/resources/truffle-config.js
@@ -1,5 +1,5 @@
 module.exports = {
       working_directory: './src/test/resources/',
-      contracts_directory: './src/main/resources/org/exoplatform/addon/wallet/contract',
+      contracts_directory: './src/main/resources/org/exoplatform/wallet/contract',
       destination_directory: './src/test/resources/'
 };
